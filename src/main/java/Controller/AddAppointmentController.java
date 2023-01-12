d324871d93e028034ad4d834c941813bb1cb6356
rjdubu <ryyguy9@gmail.com>
2023-01-12 15:06:29 -0700

    AddAppointment Controller filled out for the majority, will troubleshoot once I connect it to the Main appointment controller

--- src/main/java/Controller/AddAppointmentController.java
+++ src/main/java/Controller/AddAppointmentController.java
@@ -1,4 +1,134 @@
 package Controller;
 
-public class AddAppointmentController {
+import Model.Customer;
+import Model.DataBaseAppointment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.sql.Time;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class AddAppointmentController implements Initializable {
+
+    @FXML private ComboBox ContactCombo;
+    @FXML private
+    Label currentTime;
+    @FXML private Label NYLabel;
+    @FXML private Label PhoenixLabel;
+    @FXML private Label LondonLabel;
+    @FXML private TextField CustomerNameField;
+    @FXML private TextField ContactField;
+    @FXML private TextField LocationField;
+    @FXML private DatePicker DateField;
+    @FXML private ComboBox TimeComboBox;
+    @FXML private ComboBox ApTypeCombo;
+
+
+    private Customer customer;
+    private final ObservableList<String> appointmentTimes = FXCollections.observableArrayList("9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00");
+    private final ObservableList<String> appointmentTypes = FXCollections.observableArrayList(
+            "Insurance Quote", "Prescription List Check", "Plan Change", "Medicare Questions", "Life Insurance");
+
+    private final ObservableList<String> contacts = FXCollections.observableArrayList("Jim", "Jody", "Chris");
+    private ObservableList<String> errorTypes = FXCollections.observableArrayList();
+    public boolean contactValidator(int appointmentContact){
+        if(appointmentContact == -1){
+            errorTypes.add("Please select a contact");
+            return false;
+        }
+        return true;
+    }
+    public boolean typeValidator(int appointmentType){
+        if(appointmentType == -1){
+            errorTypes.add("Please select an appointment");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public boolean timeValidator(int appointmentTime){
+        if(appointmentTime == -1){
+            errorTypes.add("Please select and appointment time");
+            return false;
+        }else return true;
+    }
+    public boolean dateValidator(LocalDate appointmentDate){
+        if(appointmentDate == null){
+            errorTypes.add("Please select an appointment date");
+            return false;
+        }else return true;
+    }
+    public String displayErrorMessages(){
+        String string = "";
+        if(errorTypes.size() > 0){
+            for(String error : errorTypes){
+                string = string.concat(error);
+            }
+            return string;
+        }else {
+            string = "Database Error";
+            return string;
+        }
+    }
+    public boolean handleAddAppointment(int id){
+        errorTypes.clear();
+        int appointmentContact = ContactCombo.getSelectionModel().getSelectedIndex();
+        int appointmentType = ApTypeCombo.getSelectionModel().getSelectedIndex();
+        int appointmentTime = TimeComboBox.getSelectionModel().getSelectedIndex();
+        LocalDate localDate = DateField.getValue();
+        if(!contactValidator(appointmentContact) || !typeValidator(appointmentType) || !timeValidator(appointmentTime) || dateValidator(localDate)){
+            return false;
+        }
+        if(DataBaseAppointment.appointmentsOverlap(-1, LocationField.getText(), localDate.toString(), appointmentTimes.get(appointmentTime))){
+            errorTypes.add("Appointments Overlap");
+            return false;
+        }
+        if(DataBaseAppointment.saveAppointment(id, appointmentTypes.get(appointmentType), contacts.get(appointmentContact), LocationField.getText(), localDate.toString(), appointmentTimes.get(appointmentTime))){
+            return true;
+        }else {
+            errorTypes.add("Database Error");
+            return false;
+        }
+    }
+
+
+
+    public void populateCustomerNameColumn(String name){
+        CustomerNameField.setText(name);
+    }
+
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ContactCombo.setItems(contacts);
+        TimeComboBox.setItems(appointmentTimes);
+        ApTypeCombo.setItems(appointmentTypes);
+        DateField.setDayCellFactory(datePicker -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate DateField, boolean none) {
+                super.updateItem(DateField, none);
+                setDisable(none || DateField.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                        DateField.getDayOfWeek() == DayOfWeek.SUNDAY ||
+                        DateField.isBefore(LocalDate.now()));
+                if (DateField.getDayOfWeek() == DayOfWeek.SATURDAY
+                        || DateField.getDayOfWeek() == DayOfWeek.SUNDAY ||
+                        DateField.isBefore(LocalDate.now())) {
+                    setStyle("-fx-background-color: #fa0400");
+                }
+            }
+
+        });
+    }
 }
--- src/main/java/Controller/MainAppointmentController.java
+++ src/main/java/Controller/MainAppointmentController.java
@@ -1,34 +1,79 @@
 package Controller;
 
+import Model.Appointment;
+import Model.Customer;
+import com.example.wilkinson_c195.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainAppointmentController implements Initializable {
 
     @FXML private
-    TableView CustomerTableView;
-    @FXML private TableColumn CustomerIDColumn;
-    @FXML private TableColumn CustomerColumn;
-    @FXML private TableView MonthlyApptTableView;
-    @FXML private TableColumn MonthlyLocationCol;
-    @FXML private TableColumn MonthlyDescriptionCol;
-    @FXML private TableColumn MonthlyContactCol;
-    @FXML private TableColumn MonthlyStartCol;
-    @FXML private TableColumn MonthlyEndCol;
+    TableView<Customer> CustomerTableView;
+    @FXML private TableColumn<Customer, Integer> CustomerIDColumn;
+    @FXML private TableColumn<Customer, String> CustomerColumn;
+    @FXML private TableView<Appointment> MonthlyApptTableView;
+    @FXML private TableColumn<Appointment, String> MonthlyLocationCol;
+    @FXML private TableColumn<Appointment, String> MonthlyDescriptionCol;
+    @FXML private TableColumn<Appointment, String> MonthlyContactCol;
+    @FXML private TableColumn<Appointment, String> MonthlyStartCol;
+    @FXML private TableColumn<Appointment, String> MonthlyEndCol;
     @FXML private Tab WeeklyTab;
-    @FXML private TableView WeeklyTableView;
-    @FXML private TableColumn WeeklyDescriptionCol;
-    @FXML private TableColumn WeeklyContactCol;
-    @FXML private TableColumn WeeklyStartCol;
+    @FXML private TableView<Appointment> WeeklyTableView;
+    @FXML private TableColumn<Appointment, String> WeeklyDescriptionCol;
+    @FXML private TableColumn<Appointment, String> WeeklyContactCol;
+    @FXML private TableColumn<Appointment, String> WeeklyStartCol;
+    @FXML private AnchorPane AppointmentAnchorMain;
+    private boolean monthlyBool;
+    private Appointment selectedAppointment;
+    private Customer selectedCustomer;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onActionAddAppointment(ActionEvent actionEvent) throws IOException {
+        if(CustomerTableView.getSelectionModel().getSelectedItem() != null){
+            selectedCustomer = CustomerTableView.getSelectionModel().getSelectedItem();
+
+        }else {
+            return;
+        }
+        Dialog<ButtonType> dialog = new Dialog();
+        dialog.initOwner(AppointmentAnchorMain.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(Main.class.getResource("AddAppointment.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch (IOException ioException){
+            System.out.println("Error Adding Appointment: " + ioException.getMessage());
+        }
+        ButtonType save = new ButtonType("Save", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(save);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        AddAppointmentController addAppointmentController = fxmlLoader.getController();
+        //add appointment controller finish so i can add it
+        addAppointmentController
+    }
+
+    public void onActionModifyAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onActionDeleteAppointment(ActionEvent actionEvent) {
+    }
+
+    public void onActionBack(ActionEvent actionEvent) {
     }
 }
